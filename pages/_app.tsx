6f4c241874d66cc5678c7d5068fd855ff2e8bf90
shmuel <shmuelpro@gmail.com>
2021-09-24 09:15:28 +0300

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import * as React from 'react';
+import Head from 'next/head';
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '../src/theme';
+import createEmotionCache from '../src/createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+interface IProps extends AppProps {
+  emotionCache: EmotionCache;
+}
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: IProps) {
+  return (<CacheProvider value={emotionCache}>
+    <Head>
+      <title>{pageProps.title}</title>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </CacheProvider>)
+
 }
 export default MyApp
